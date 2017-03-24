122e717cc3460c4ab6081c537d0e186edbd5ace0
ThisIsQasim <QasRoX@hotmail.com>
2017-03-24 20:08:19 +0500

    Automatic db config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,12 @@ set -e
 if [ ! -e '/var/www/html/index.php' ]; then
 	tar cf - --one-file-system -C /usr/src/tastyigniter . | tar xf -
 	chown -R www-data /var/www/html
+	if [ -n "${!MYSQL_ENV_MYSQL_*}" ]; then
+		sed -i -e 's/localhost/db/g' /var/www/html/system/tastyigniter/config/database.php
+		sed -i -e 's/your database username/${MYSQL_USER}/g' /var/www/html/system/tastyigniter/config/database.php
+		sed -i -e 's/your database password/${MYSQL_PASSWORD}/g' /var/www/html/system/tastyigniter/config/database.php
+		sed -i -e 's/your database name/${MYSQL_DATABASE}/g' /var/www/html/system/tastyigniter/config/database.php
+	fi
 fi
 
 exec "$@"
