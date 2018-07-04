4fbc59127fa9b9bd4de86668a491d1da4a0e39c0
Qasim <qasim.mehmood@jeeny.me>
2018-07-04 17:58:59 +0500

    auto db config fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,11 +4,11 @@ set -e
 if [ ! -e '/var/www/html/index.php' ]; then
 	tar cf - --one-file-system -C /usr/src/tastyigniter . | tar xf -
 	chown -R www-data /var/www/html
-	if [ -n "${!MYSQL_ENV_MYSQL_*}" ]; then
-		sed -i -e 's/localhost/db/g' /var/www/html/system/tastyigniter/config/database.php
-		sed -i -e 's/your database username/${MYSQL_USER}/g' /var/www/html/system/tastyigniter/config/database.php
-		sed -i -e 's/your database password/${MYSQL_PASSWORD}/g' /var/www/html/system/tastyigniter/config/database.php
-		sed -i -e 's/your database name/${MYSQL_DATABASE}/g' /var/www/html/system/tastyigniter/config/database.php
+	if [ -n "${MYSQL_HOSTNAME}" ]; then
+		sed -i -e "s/localhost/$MYSQL_HOSTNAME/g" /var/www/html/system/tastyigniter/config/database.php
+		sed -i -e "s/your database username/$MYSQL_USER/g" /var/www/html/system/tastyigniter/config/database.php
+		sed -i -e "s/your database password/$MYSQL_PASSWORD/g" /var/www/html/system/tastyigniter/config/database.php
+		sed -i -e "s/your database name/$MYSQL_DATABASE/g" /var/www/html/system/tastyigniter/config/database.php
 	fi
 fi
 
